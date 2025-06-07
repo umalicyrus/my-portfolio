68b3b5d7ce155f7eecbfb7029bfaa2e276005bdd
cyrusumali <umalicyrus04171998@gmail.com>
2025-06-07 09:54:32 +0800

    add responsive Navbar with mobile menu and custom fonts

---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import {Outfit, Ovo } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-outfit", // <- important!
+});
+
+const ovo = Ovo({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-ovo", // <- important!
+});
+
+export const metadata = {
+  title: "Portfolio - Cyrus P. Umali",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+        <body className={`${outfit.variable} ${ovo.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
