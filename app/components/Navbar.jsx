971191f1ab74900d552df409f34f64539c0adcfc
cyrusumali <umalicyrus04171998@gmail.com>
2025-06-10 13:25:07 +0800

    Fix navbar overlapping issue and add scroll-based background color change

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import { assets } from '@/assets/assets'
+import { ZCOOL_KuaiLe } from 'next/font/google';
 import Image from 'next/image'
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 function Navbar() {
-
+  const [isScroll, setIsScroll] = useState(false)
   const sideMenuRef = useRef();
 
   const openMenu = ()=> {
@@ -12,18 +13,27 @@ function Navbar() {
   const closeMenu = ()=> {
     sideMenuRef.current.style.transform = 'translateX(16rem)'
   }
-
+  useEffect(()=>{
+    window.addEventListener('scroll',()=>{
+      if(scrollY > 50){
+        setIsScroll(true)
+      }else{
+        setIsScroll(false)
+      }
+    })
+  },[])
+    
   return (
     <>
     <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
       <Image src={assets.header_bg_color} alt='' className='w-full'/>
     </div>
-      <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
+      <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""}`}>
         <a href="#top">
           <Image src={assets.logo} className='w-40 cursor-pointer mr-14'alt=''/>
         </a>
 
-        <ul className='hidden md:flex items-center gap-6 lg-gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
+        <ul className={`hidden md:flex items-center gap-6 lg-gap-8 rounded-full px-12 py-3 ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50"} `}>
           <li><a href="#top" className='font-ovo'>Home</a></li>
           <li><a href="#about" className='font-ovo' >About me</a></li>
           <li><a href="#services" className='font-ovo'>Services</a></li>
